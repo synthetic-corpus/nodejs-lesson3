d453f3c477406cb19812341659963192964f68ca
Syn <joel.gonzaga@gmail.com>
2017-08-24 10:48:53 -0700

    added some notes and comments after watching the video a second time.

--- notes.js
+++ notes.js
@@ -2,6 +2,28 @@ console.log('Starting notes.js');
 
 var addNote = (title, body) => {
   console.log('Adding note', title, body);
+  let notes = [];
+  // Key Value pairs here will be 'title: title'
+  let note = {
+    title,
+    body
+  };
+
+  try {
+    // "Try" is used here because the Json file may not exist
+    var notestring = fs.readFileSync('notes-data.json');
+  } catch (e) {
+    // Do nothing on an error
+  }
+  // Unsure what this line does right now
+  // Function gets called per item in array.
+  // Fucntion returns T/F. If True.. add to the array "duplicateNotes".
+  var duplicateNotes = notes.filters((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
