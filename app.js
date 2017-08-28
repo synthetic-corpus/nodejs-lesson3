1e87e7afde5918c7a1dc4b0e2dbda8771a78e240
Syn <joel.gonzaga@gmail.com>
2017-08-28 16:45:57 -0700

    installed Yargs. (silly, I know) and corrected a syntax problem. Commands work as expected.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv
+const argv = yargs
   .command('add','Add a new note',{
     title: {
       describe: "The Title of this note",
@@ -19,7 +19,7 @@ const argv = yargs.argv
   })
   .help()
   .argv;
-  
+
 var command = argv._[0];
 
 if (command === 'add') {
